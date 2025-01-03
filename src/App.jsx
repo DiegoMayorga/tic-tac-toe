c3ed50c7a4992576decd94018af5372e6c8f9183
DiegoMayorga <diegoamayorgat@gmail.com>
2025-01-03 09:59:24 -0500

    Game Over screen and checking for a draw

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -35,6 +36,27 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   const handleSelectSquare = (rowIndex, colIndex) => {
     /* setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X")); */
     setGameTurns((prevTurns) => {
@@ -65,6 +87,7 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
+          {(winner || hasDraw) && <GameOver winner={winner} />}
           <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns} />
