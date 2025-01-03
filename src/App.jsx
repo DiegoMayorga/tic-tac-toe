7578cb65e52029d825fdab3d3f6720233c1dde19
DiegoMayorga <diegoamayorgat@gmail.com>
2025-01-03 09:19:26 -0500

    Lifting computed values up and adding winning-combinations file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -18,6 +25,15 @@ function App() {
   /* const [activePlayer, setActivePlayer] = useState("X"); */
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
 
   const handleSelectSquare = (rowIndex, colIndex) => {
     /* setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X")); */
@@ -49,7 +65,7 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns} />
       </main>
